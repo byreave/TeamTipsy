7e63d5fe25f0ae53bd01abc230e00bdd9653a163
Byreave Luo <654297651@qq.com>
2018-10-18 19:39:42 -0600

    add teleport and motion controller

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class CharacterControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
+        //this.transform.Translate(interactionSourceState);
+        foreach (var interSourceState in interactionSourceStates)
+        {
+            //if(interSourceState.thumbstickPosition)
+            //interSourceState.
+            Debug.Log(interSourceState.thumbstickPosition);
+        }
+	}
+}
