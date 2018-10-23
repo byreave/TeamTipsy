dbc4bb95e3edb9e1d9ff98e1f43d1ccaaac518a7
Byreave Luo <654297651@qq.com>
2018-10-23 12:33:57 -0600

    add drunk bar and drag mechanic

---
+++ Assets/Scripts/DrunkBarControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrunkBarControl : MonoBehaviour {
+
+    public float DrunkLevel = 0.0f;
+    [SerializeField]
+    private Slider DrunkSlider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DrunkSlider.value = DrunkLevel;
+	}
+}
