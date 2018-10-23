c32ee4795b122390c7f4b8f74f59721276ea301e
Byreave Luo <654297651@qq.com>
2018-10-23 17:19:04 -0600

    add fading effect

---
+++ Assets/Scripts/FadeEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeEffect : MonoBehaviour {
+
+    // Use this for initialization
+    private Component[] renderers;
+	void Start () {
+        renderers = this.GetComponentsInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(MeshRenderer r in renderers)
+        {
+            r.material.color = new Color(r.material.color.r, r.material.color.g, r.material.color.b, 0.1f);
+        }
+		
+	}
+}
