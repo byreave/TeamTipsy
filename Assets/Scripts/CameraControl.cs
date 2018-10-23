7a530744e00515c5784010b59300dbc43dc938e6
Byreave Luo <654297651@qq.com>
2018-10-23 16:50:27 -0600

    fix the camera pos

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector3 OriginalPos;
+    void Start()
+    {
+        OriginalPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = OriginalPos;
+    }
+}
--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,20 +5,14 @@ using UnityEngine.XR.WSA.Input;
 
 public class CharacterControl : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    private Vector3 OriginalPos;
 	void Start () {
-		
+        OriginalPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var interactionSourceStates = InteractionManager.GetCurrentReading();
-        //this.transform.Translate(interactionSourceState);
-        foreach (var interSourceState in interactionSourceStates)
-        {
-            //if(interSourceState.thumbstickPosition)
-            //interSourceState.
-            Debug.Log(interSourceState.thumbstickPosition);
-        }
+        this.transform.position.Set(0.0f, 0.0f, 0.0f);
 	}
 }
