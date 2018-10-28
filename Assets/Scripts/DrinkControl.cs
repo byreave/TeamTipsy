b0a736c0e902cb39213fcdeec80cbd916c965662
Pinak Jalan <pinak.jalan@me.com>
2018-10-28 16:07:06 -0600

    Cleaned some code, added assets, working on drunk level and illusions

--- Assets/Scripts/DrinkControl.cs
+++ Assets/Scripts/DrinkControl.cs
@@ -6,13 +6,9 @@ public class DrinkControl : MonoBehaviour {
 
     // Use this for initialization
     public GameObject Player;
-    public GameObject alternateWorld;
-    public GameObject weirdWorld;
 
-    private FadeWhitebox fd;
-    private FadeWhitebox fd1;
+    
     private float drunkLevel;
-    private float otherLevel;
 
     private float timer;
     private bool hasTimerStarted;
@@ -21,13 +17,7 @@ public class DrinkControl : MonoBehaviour {
 
     private void Start()
     {
-        fd = alternateWorld.GetComponent<FadeWhitebox>();
-        fd1 = weirdWorld.GetComponent<FadeWhitebox>();
-
-        otherLevel = 0.0f;
-        fd1.alphaLevel = otherLevel;
         drunkLevel = 1.0f;
-        fd.alphaLevel = drunkLevel;
 
         timer = 0.0f;
         increaseAlpha = false;
@@ -37,16 +27,7 @@ public class DrinkControl : MonoBehaviour {
 
     private void Update()
     {
-
-        if (increaseAlpha == true)
-        {
-            if (fd.alphaLevel <= 1.0f)
-            {
-                fd.alphaLevel += 0.002f;
-
-            }
-        }
-
+       
         if(hasTimerStarted == true)
         {
 
@@ -58,36 +39,35 @@ public class DrinkControl : MonoBehaviour {
                 hasTimerStarted = false;
             }
         }
-
-        if(fd.alphaLevel == 1.0f)
-        {
-            increaseAlpha = false;
-        }
-
         
     }
     // Use this for initialization
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Drink"))
+        if (other.CompareTag("Drink"))
         {
             Player.GetComponent<DrunkBarControl>().DrunkLevel += 30;
-            other.gameObject.SetActive(false);
-       
-
-            fd.alphaLevel -= 0.3f;
-            fd1.alphaLevel += 0.4f;
 
             timer = 0.0f;
             hasTimerStarted = true;
 
         }
 
+        if (other.CompareTag("Glass"))
+        {
+            if (other.gameObject.GetComponent<GlassControl>().FillLevel > 0)
+            {
+                other.gameObject.GetComponent<GlassControl>().FillLevel--;
+                //to do: Increase the drunk level here.
+
+            }
+        }
     }
+ 
     private void OnTriggerStay(Collider other)
     {
         //if drunk, the glass's water level should be reduced.
-        if (other.CompareTag("Glass"))
+       /* if (other.CompareTag("Glass"))
         {
             //currently don't rotate.
             //if(other.gameObject.transform.rotation.x > 0.75f || other.gameObject.transform.rotation.x < -0.75f)
@@ -99,6 +79,6 @@ public class DrinkControl : MonoBehaviour {
 
                 }
             }
-        }
+        }*/
     }
 }
