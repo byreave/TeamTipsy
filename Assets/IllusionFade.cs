23e1c669007d9b71e4cb55d24c8573889954ea6b
Pinak Jalan <pinak.jalan@me.com>
2018-10-28 16:31:22 -0600

    Added basic alpha fading

---
+++ Assets/IllusionFade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IllusionFade : MonoBehaviour {
+
+    public bool isFading;
+	// Use this for initialization
+	void Start () {
+        isFading = false;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Alpha: " + gameObject.GetComponent<SpriteRenderer>().color.a);
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+
+    }
+}
