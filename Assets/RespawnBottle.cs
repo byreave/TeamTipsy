f97cc3a256fcba5df4719e0eaae58a7ff64d39e9
Pinak Jalan <pinak.jalan@me.com>
2018-10-31 21:11:11 -0600

    Added absinthe bottle respawn

---
+++ Assets/RespawnBottle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBottle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            Instantiate(gameObject, new Vector3(-0.4f, -0.3f, -2.6f), Quaternion.identity);
+        }
+    }
+}
